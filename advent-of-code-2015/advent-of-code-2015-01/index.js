fd273053f960024e706fd666828ab83ea68d69b1
Marine Heckler <marine.heckler@gmail.com>
2020-10-26 18:17:26 +0100

    Advent of code 2015 - Day 1

--- advent-of-code-2015/advent-of-code-2015-01/index.js
+++ advent-of-code-2015/advent-of-code-2015-01/index.js
@@ -1,36 +1,51 @@
-// jshint esversion: 6
-
-// Link to the challenge
+// Advent of code challenge 2015
 // https://adventofcode.com/2015/day/1
 
 const fs = require('fs');
 
+// First question - To what floor do the instructions take Santa?
+const findFloor = input => {
+    let currentFloor = 0;
+
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] === '(') {
+            currentFloor++;
+        } else if (input[i] === ')') {
+            currentFloor--;
+        }
+    }
+
+    return currentFloor;
+};
+
+// Second question - Find the position of the first character that
+// causes him to enter the basement (floor -1)
+const findPosition = input => {
+    let currentFloor = 0;
+
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] === '(') {
+            currentFloor++;
+        } else if (input[i] === ')') {
+            currentFloor--;
+        }
+
+        if (currentFloor === -1) return i + 1;
+    }
+};
+
 fs.readFile('./input.txt', (err, data) => {
+    if (err) {
+        console.log("Couldn't read the input.");
+    } else {
+        const lastFloor = findFloor(data.toString());
+        const firstCharacter = findPosition(data.toString());
 
-  // Save the input
-  const input = data.toString();
-
-  // Init answers
-  let floor = 0;
-  let firstTime = true;
-  let characPosition = 1;
-
-  for (let i = 0; i < input.length; i++) {
-
-    // Question 1 - Last floor
-    if (input[i] === '(') {
-      floor++;
-    } else if (input[i] === ')') {
-      floor--;
+        console.log(
+            'Question 1 :',
+            lastFloor,
+            '// Question 2 :',
+            firstCharacter
+        );
     }
-
-    // Question 2 - First time in floor -1
-    if (floor < 0 && firstTime === true) {
-      firstTime = false;
-      characPosition += i;
-    }
-  }
-
-  console.log('Final floor :', floor);
-  console.log('Character position :', characPosition);
 });
