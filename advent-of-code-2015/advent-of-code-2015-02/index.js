43180f4733f47942c29f403bddbd69678bc70e93
Marine Heckler <marine.heckler@gmail.com>
2020-10-27 11:39:57 +0100

    Advent of code 2015 - Day 2

--- advent-of-code-2015/advent-of-code-2015-02/index.js
+++ advent-of-code-2015/advent-of-code-2015-02/index.js
@@ -1,35 +1,70 @@
-// jshint esversion: 6
-
-// Link to the challenge
+// Advent of code challenge 2015
 // https://adventofcode.com/2015/day/2
 
 const fs = require('fs');
 
+// Input convertion
+
+const convertInput = input => {
+    return input
+        .split('\n')
+        .map(present => present.split('x').map(dimension => Number(dimension)));
+};
+
+// Question 1 - How many total square feet of wrapping paper
+// should the elves order?
+
+const calculatePaperSize = input => {
+    const inputArray = convertInput(input);
+    let total = 0;
+
+    inputArray.forEach(presentDimensions => {
+        const area1 = presentDimensions[0] * presentDimensions[1];
+        const area2 = presentDimensions[1] * presentDimensions[2];
+        const area3 = presentDimensions[2] * presentDimensions[0];
+
+        const smallestArea = Math.min(area1, area2, area3);
+
+        total += 2 * (area1 + area2 + area3) + smallestArea;
+    });
+
+    return total;
+};
+
+// Question 2 - How many total feet of ribbon should they order?
+
+const calculateRibbonLength = input => {
+    const inputArray = convertInput(input);
+    let total = 0;
+
+    inputArray.forEach(presentDimensions => {
+        const perim1 = 2 * (presentDimensions[0] + presentDimensions[1]);
+        const perim2 = 2 * (presentDimensions[1] + presentDimensions[2]);
+        const perim3 = 2 * (presentDimensions[2] + presentDimensions[0]);
+
+        const bowLength =
+            presentDimensions[0] * presentDimensions[1] * presentDimensions[2];
+
+        total += Math.min(perim1, perim2, perim3) + bowLength;
+    });
+
+    return total;
+};
+
+// Main running
+
 fs.readFile('./input.txt', (err, data) => {
-  const input = data.toString();
-  const inputArray = input.split('\n');
-  inputArray.pop();
-  let totalArea = 0;
-  let totalLength = 0;
+    if (err) {
+        console.log("Couldn't read the input.");
+    } else {
+        const totalPaperSize = calculatePaperSize(data.toString());
+        const totalRibbonLength = calculateRibbonLength(data.toString());
 
-  for (let i = 0; i < inputArray.length; i++) {
-      const dimArray = inputArray[i].split('x');
-
-      const area1 = dimArray[0] * dimArray[1];
-      const area2 = dimArray[1] * dimArray[2];
-      const area3 = dimArray[2] * dimArray[0];
-
-      const smallestArea = Math.min(area1, area2, area3);
-      totalArea += (area1*2 + area2*2 + area3*2 + smallestArea);
-
-      const perim1 = 2*dimArray[0] + 2*dimArray[1];
-      const perim2 = 2*dimArray[1] + 2*dimArray[2];
-      const perim3 = 2*dimArray[2] + 2*dimArray[0];
-
-      const smallestPerim = Math.min(perim1, perim2, perim3);
-      totalLength += (smallestPerim + (dimArray[0]*dimArray[1]*dimArray[2]));
-  }
-
-  console.log('Total area :', totalArea);
-  console.log('Total length :', totalLength);
+        console.log(
+            'Question 1 :',
+            totalPaperSize,
+            '// Question 2 :',
+            totalRibbonLength
+        );
+    }
 });
